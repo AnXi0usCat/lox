44eaf65450ddbafb67f73eb8b98bbf3f11decdd6
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2026-01-03 10:56:23 +0000

    inital commit

---
+++ jlox/src/repl.rs
@@ -0,0 +1,6 @@
+use std::error::Error;
+
+
+pub fn start_repl() -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
---
+++ jlox/target/debug/deps/jlox-99559e306cf3ed04.d
@@ -0,0 +1,5 @@
+/Users/mihhailf/rust/lox/jlox/target/debug/deps/jlox-99559e306cf3ed04.d: src/lib.rs
+
+/Users/mihhailf/rust/lox/jlox/target/debug/deps/libjlox-99559e306cf3ed04.rmeta: src/lib.rs
+
+src/lib.rs:
