31e95eeff6414d9071d51caa558058b966269f66
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2026-01-03 11:09:32 +0000

    remove

--- jlox/target/debug/deps/jlox-19e1126094df01e2.d
+++
@@ -1,6 +0,0 @@
-/Users/mihhailf/rust/lox/jlox/target/debug/deps/jlox-19e1126094df01e2.d: src/main.rs src/repl.rs
-
-/Users/mihhailf/rust/lox/jlox/target/debug/deps/libjlox-19e1126094df01e2.rmeta: src/main.rs src/repl.rs
-
-src/main.rs:
-src/repl.rs:
--- jlox/target/debug/deps/jlox-7332a03d5a327b5a.d
+++
@@ -1,5 +0,0 @@
-/Users/mihhailf/rust/lox/jlox/target/debug/deps/jlox-7332a03d5a327b5a.d: src/lib.rs
-
-/Users/mihhailf/rust/lox/jlox/target/debug/deps/libjlox-7332a03d5a327b5a.rmeta: src/lib.rs
-
-src/lib.rs:
