a7f0e04bd9a1bcbdcd2ed907922949910e2743df
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2026-01-04 13:24:19 +0000

    token

--- jlox/src/token.rs
+++ jlox/src/token.rs
@@ -97,3 +97,24 @@ impl Display for TokenType {
         write!(f, "{}", name)
     }
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Option<String>,
+    line: usize,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.literal {
+            Some(ref val) => write!(
+                f,
+                "{}",
+                format!("[{} {} {}]", self.token_type, self.lexeme, val)
+            ),
+            None => write!(f, "{}", format!("[{} {}]", self.token_type, self.lexeme)),
+        }
+    }
+}
